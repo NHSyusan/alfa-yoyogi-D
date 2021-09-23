ef6879152f2933af940be15d3097e9958d70ade3
Yutaro_NHS <89887035+NHSyusan@users.noreply.github.com>
2021-09-24 08:42:15 +0900

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -21,7 +21,7 @@ const app = Vue.createApp({
             q1: '607',
           },
           stage2: {
-            q1: 'じゅうでんぎれ',
+            q1: 'battery out',
             // q2: 'えええ',
             // q3: 'おおお'
           },
